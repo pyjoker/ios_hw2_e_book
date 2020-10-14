8113b57ac1a5e896147eae5b684f029494cb6c7b
pyjoker <karta1597596@gmail.com>
2020-10-14 11:16:43 +0800

    Initial Commit

---
+++ ios_hw2_e_book/ios_hw2_e_bookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_hw2_e_bookApp.swift
+//  ios_hw2_e_book
+//
+//  Created by mac on 2020/10/14.
+//
+
+import SwiftUI
+
+@main
+struct ios_hw2_e_bookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
